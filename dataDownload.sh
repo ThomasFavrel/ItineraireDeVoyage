c8c17153ce46ee97972c0c0d078e7a38c9970698
ThomasFavrel <tfavrel97@gmail.com>
2023-05-31 06:51:43 +0200

    add script data

---
+++ dataDownload.sh
@@ -0,0 +1,5 @@
+curl --compressed -X GET "https://diffuseur.datatourisme.fr/webservice/7f2fa3ea017a3b4609a2be86f545bb25/523dc2bd-0eaf-4d95-bded-0c7b96b90e31" --output data.zip
+
+unzip data.zip -d Data/JSON2/
+
+rm data.zip
